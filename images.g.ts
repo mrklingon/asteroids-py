8e4072ee72648a1a173e6f19157c85a94d3557f9
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-08-10 19:58:57 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,113 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image5":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "as1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f . . . . . . . . . 
+. . . f f b f . . . 1 . f . . . 
+. . . f b b f f . . f f f f . . 
+. . f f b b b f f f f b f . . . 
+. . f b b b b b b b b b f . . . 
+. . f b b b b f b b b b f . . . 
+. . f b b b b f f b b f f . . . 
+. . f b f f b b f b f f . . . . 
+. . f b f b b b b f f . . . . . 
+. . f f f b b b b f f . . . . . 
+. . . . f b b b b b f f . . . . 
+. . . . f b b b f f f . . . . . 
+. . . . f f f f . . . . . . . . 
+`;
+            case "image2":
+            case "as2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. f f f f f f . . . . . . . . . 
+. f d d d d f f f f . . . . . . 
+. f d d d d d d d f f . . . . . 
+. f d d d d d d d d f . . . . . 
+. f d f f d d d d f f . . . . . 
+. f d d d f f d f f f . . . . . 
+f f d d d d d d f d f f . . . . 
+f f d d f d d d d d d f f . . . 
+. . f f f f f f d d d d f . . . 
+. . . . . . f d d d d d f . . . 
+. . . . . . f f d d d f f . . . 
+. . . . . . . . f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "as0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . f b b b f f . . f . . 
+. . . . . f f b b b f f f f f . 
+. . . . . . f b b b b f f b f . 
+. . . . . . f b b f f b b b f . 
+. . . . . f f b f f b b b b b f 
+. . . f f f b b b b b b b b b f 
+. . . f b b b b b b f b b b b f 
+. . . f f b b b b b f f f f f f 
+. . . . f f f b b b b b f . . . 
+. . . . . . f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "as3":return img`
+. . . . . . . . f f . . . . . . 
+. . f f f f f f f f . . . . . . 
+. . f d d d d d d d f . . . . . 
+. . f d d d f d d d f . . . . . 
+. . f d d d f d d f f . . . . . 
+. . f d d d d f d d f . . . . . 
+. . f f d d d f d d f f f . . . 
+. . . f d d d d d d f d f . . . 
+. . . f d d d f f d d d d f . . 
+. . . f f d f f d d d d d f . . 
+. . . . f f f f f d d d d f . . 
+. . . . . . . . f f d d f f . . 
+. . . . . . . . . f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
